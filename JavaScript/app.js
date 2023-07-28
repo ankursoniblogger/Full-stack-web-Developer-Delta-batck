a0bf57b68bf6b2dab4391fc4e6c5a65dab5a733c
ankursoniblogger <ankursoniblogger@gmail.com>
2023-07-28 08:59:45 +0530

    JavaScript
    In this section , part 1 part 2 are included

---
+++ JavaScript/app.js
@@ -0,0 +1,99 @@
+// assignment question part 2 
+// question 1 
+
+// let num = 10 ;
+
+// if(num % 2 == 0){
+//     console.log("Good");
+// } else{
+//     console.log("bad");
+// }
+
+// question 2 
+
+// let name = prompt(" what is your name ?");
+// let age = prompt(" what is your age ?");
+// alert(`Your name is ${name} and you are  ${age} years old. :) `);
+// console.log(name);
+// console.log(age);
+
+
+// question 3 switch statment
+
+
+// switch () {
+//     case 1 :  console.log("Jan , feb , march , april ");
+//         break;
+//     case 2 :  console.log("Jan , feb , march , april ");
+//     default:
+//         break;
+// }
+
+
+// question 4 
+
+// let str = "ankur soni";
+
+// if((str.charAt(0) == "A" || str.charAt(0) == "a" ) && str.length > 5 ){
+//         console.log("Golden string");
+// } else {
+//     console.log("NOt golden string ");
+// };
+
+// question 5 
+
+// let num1 = 20 ;
+// let num2 = 18 ;
+// let num3 = 19 ;
+
+
+// if(num1 > num2 && num1 > num3){
+//     console.log("Num1 is greater than all of these ");
+// } else if(num2 >num3 ){
+//     console.log("Num2 is greater ");
+// } else{
+//     console.log("Num 3 is greater");
+// }
+
+// question 6 bonus question 
+/*
+let num1 = "31212223";
+// console.log(num1.charAt(num1.length-1));
+let num2 = "47852123";
+// console.log(num2.charAt(num2.length-1));
+
+if(num1.charAt(num1.length-1) == num2.charAt(num2.length-1)){
+    console.log("There last digit are same");
+} else{
+    console.log("Last digit are diff");
+}*/
+
+
+// let str = "Ankur soni";
+// console.log(str[0]);
+
+
+// javaScript part 3  
+// practice question 
+
+// Question 1
+// let msg = 'help!' ;
+// console.log(msg.trim().toUpperCase());
+
+// question 2
+// let name = "ApnaCollege " ;
+// console.log(name.slice(4,9));
+// console.log(name.indexOf("na"));
+// console.log(name.replace("Apna" , "Our"));
+
+
+// practice question 
+let month = ["january" , "july" , "march", "augest"];
+
+// console.log(month.splice(0,2, "july","june"));
+
+// Array method 
+
+let cars = ["maruti", "xuv", "toyota", "farri" ];
+
+// console.log(cars);
