b8cac96f6d1d5bc37aa356f5ca92c9951e4ac128
ankursoniblogger <ankursoniblogger@gmail.com>
2023-08-07 20:09:00 +0530

    java script

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -88,13 +88,13 @@ if(num1.charAt(num1.length-1) == num2.charAt(num2.length-1)){
 
 
 // practice question 
-let month = ["january" , "july" , "march", "augest"];
+// let month = ["january" , "july" , "march", "augest"];
 
 // console.log(month.splice(0,2, "july","june"));
 
 // Array method 
 
-let cars = ["maruti", "xuv", "toyota", "farri" ];
+// let cars = ["maruti", "xuv", "toyota", "farri" ];
 
 // console.log(cars);
 
@@ -103,18 +103,18 @@ let cars = ["maruti", "xuv", "toyota", "farri" ];
 // practice question part 3 
 // nesting array 
 
-let game = [ ["X" , null , "O"], [null, "X" , null], ["O" , null , "X"] ];
+// let game = [ ["X" , null , "O"], [null, "X" , null], ["O" , null , "X"] ];
 
 
 // Assignment question part 3 
 
-let arr = [7,9,0 ,-2];
-let n = 3;
+// let arr = [7,9,0 ,-2];
+// let n = 3;
 
-let ans = arr.slice(0,n);
+// let ans = arr.slice(0,n);
 // console.log(ans);
 
-let ans2 = arr.slice(arr.length-n);
+// let ans2 = arr.slice(arr.length-n);
 // console.log(ans2);
 
 // question 3 Write a js program to check string are empty are not 
@@ -140,7 +140,7 @@ if(str1.length == 0){
 
 
 // question 4 
-
+/*
 
 let str = "ApNaCollege";
 let char = 5;
@@ -150,7 +150,7 @@ if(str[char] == str[char].toLowerCase()){
 } else{
     console.log("character is not lower case");
 }
-
+*/
 
 // javaScript part-4 
 
@@ -162,22 +162,55 @@ if(str[char] == str[char].toLowerCase()){
  }*/
 
 // 2nd method 
-for(let i=1; i<=15;i=i+2){
+// for(let i=1; i<=15;i=i+2){
     // console.log(i);
-}
+// }
 
 // print all even number from 2 to 10 
-for(let i=2;i<=10;i=i+2){
+// for(let i=2;i<=10;i=i+2){
     // console.log(i);
-}
+// }
 
 // print multiple table of 5
-for(let i=5;i<=50;i=i+5){
+// for(let i=5;i<=50;i=i+5){
     // console.log(i);
-}
+// }
 
 // let x = prompt("Enter a number :) ");
 // x = parseInt(x);
 // for(let i=x;i<=x*10;i=i+x){
     // console.log(i);
+// }
+
+// Favorite movie 
+/*
+let movie = "avtar";
+let guess = prompt("Guess My Favorite movie");
+
+while( (movie != guess) && (guess != "quit") ){
+    guess = prompt("Wrong guess . Please try again ");
+}
+
+if(guess == movie){
+    console.log("Congrate!! :)")
+} else{
+    console.log("You Quit the game  :( ")
+} */
+
+// uses of break and continue 
+/*
+let i = 1;
+while(i<=5){
+    // if(i == 3){
+    //     console.log("Break point 3");
+    //     break;
+    // }
+    console.log(i);
+    i++ ;
+}*/
+
+// let array = ["banana","apple", "pineapple", "grapesh"];
+
+// for (let i=0;i<array.length;i++){
+//     console.log(  i, array[i]);
 // }
