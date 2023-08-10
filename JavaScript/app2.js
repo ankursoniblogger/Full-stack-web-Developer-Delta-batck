6556d830173e40f9ded362fab4cd00b957558f13
ankursoniblogger <ankursoniblogger@gmail.com>
2023-08-10 10:35:23 +0530

    javaScript part 4 completed

--- JavaScript/app2.js
+++ JavaScript/app2.js
@@ -1,1 +1,52 @@
-console.log("hello world! ");
+// todo app bulid 😄
+/*😄
+let tudo = [];
+
+let req = prompt("Enter your request which show on window screen");
+
+while(true){
+ if(req == "quit"){
+    console.log("Queting app ")
+    break;
+ }
+ if(req == "list"){
+    console.log("----------");
+    for(let i = 0; i<tudo.length;i++){
+        console.log(i, tudo[i]);
+    }
+    console.log("--------");
+ } else if(req == "add"){
+    let task = prompt("enter , which you add ")
+    tudo.push(task);
+    console.log("tasked added");
+ } else if(req == "delete"){
+      let idx = prompt("plz enter inx , which want you delete");
+      tudo.splice(idx, 1);
+      console.log("tasked deleted");
+ }
+ req =  prompt("Enter your request which show on window screen");
+
+}
+
+😄*/
+
+
+// Assignment question 
+
+// question 1
+/*
+let arr = [1,2,3,4,5,6,2,3];
+let num = 2;
+
+for(let i=0;i<arr.length; i++){
+    if(arr[i] == num){
+       arr.splice(i,1);
+    }
+    console.log(arr[i]);
+}*/
+
+// question 2
+
+// let num = 287152;
+// let count = 6;
+
