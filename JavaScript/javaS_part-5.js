40129866dc2d853da4f7b7d2c7b73ade00f1f50e
ankursoniblogger <ankursoniblogger@gmail.com>
2023-08-13 11:56:15 +0530

    javaScript part 5

---
+++ JavaScript/javaS_part-5.js
@@ -0,0 +1,17 @@
+const student = {
+    name: "ankur soni",
+    age: 21 ,
+    marks: 80,
+    color: ["red","babypink"],
+
+};
+
+const post = {
+   username: "@ankursoniblogger",
+   content: "This is my first post ",
+   like: 150,
+   reposts: 5,
+   view: 2000,
+   tags: ["ankit", "shradha", "apna college"]
+};
+
