437062606d37bae812efb65433993ace5fdf9903
ankursoniblogger <ankursoniblogger@gmail.com>
2023-08-22 19:35:27 +0530

    Backend (node.js)
    Node.js

---
+++ Backend/math.js
@@ -0,0 +1,32 @@
+// const sum = (a,b) => a + b ;
+// const mul = (a,b) => a * b ;
+// const g = 9.8;
+// const PI = 3.14;
+
+// module.exports = "hello";
+// 1st method 
+// const obj = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI
+// }
+// 2nd method 
+// module.exports = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI
+// }
+// module.exports = obj;
+
+// 3rd mathod 
+// module.exports.sum = (a,b) => a + b ;
+// module.exports.mul = (a,b) => a * b ;
+// module.exports.g = 9.8;
+// module.exports.PI = 3.14;
+
+
+
+
+// direct directory  aceess 
---
+++ Backend/script.js
@@ -0,0 +1,30 @@
+// Node.js 
+
+// let num = 5;
+// for(let i=0;i<=num;i++){
+//     console.log("Hello ", i)
+// }
+// process property
+/*
+let argv = process.argv;
+for(let i=2; i<argv.length; i++){
+    console.log("hello to " , argv[i]);
+} */
+// console.log(process.argv);
+
+
+
+// module.exports 
+
+
+// const math = require("./math");
+// console.log(math.sum(2,3))
+// console.log(math.PI)
+
+
+
+// direct directory  aceess 
+
+const info = require("./Fruit");
+console.log(info);
+
