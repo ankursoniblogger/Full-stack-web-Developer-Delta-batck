9ffd7b287a71cd3203408f6d78659a3212d57244
ankursoniblogger <ankursoniblogger@gmail.com>
2023-09-05 16:30:46 +0530

    add some object property

--- Fronted/JavaScript/javaS_part-5.js
+++ Fronted/JavaScript/javaS_part-5.js
@@ -1,17 +1,41 @@
-const student = {
-    name: "ankur soni",
-    age: 21 ,
-    marks: 80,
-    color: ["red","babypink"],
+// const student = {
+//     name: "ankur soni",
+//     age: 21 ,
+//     marks: 80,
+//     color: ["red","babypink"],
 
-};
+// };
 
-const post = {
-   username: "@ankursoniblogger",
-   content: "This is my first post ",
-   like: 150,
-   reposts: 5,
-   view: 2000,
-   tags: ["ankit", "shradha", "apna college"]
-};
+// const post = {
+//    username: "@ankursoniblogger",
+//    content: "This is my first post ",
+//    like: 150,
+//    reposts: 5,
+//    view: 2000,
+//    tags: ["ankit", "shradha", "apna college"]
+// };
 
+// part 5 practice question 
+
+// genrate a random number btw 1 to 100 
+
+// let random = Math.floor(Math.random() * 100) +1 ;
+// console.log(random);
+
+// Assignment question 
+
+let random = Math.floor(Math.random() * 6) +1 ;
+console.log(random);
+
+// question 2 
+let car = {
+    name: "maruti",
+    model: 2020,
+    color: "white"
+}
+ 
+const person = {
+    name: "Ankur soni",
+    age: 20,
+    city: "Ayodhya"
+}
