67ff4265aeaf49fef149b4c5e384419dce98de4e
ankursoniblogger <ankursoniblogger@gmail.com>
2023-10-05 15:07:10 +0530

    add phase-3 (validation)

--- MajorProject/index.js
+++ MajorProject/index.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require('method-override');
-const ejsMate = require("ejs-mate")
+const ejsMate = require("ejs-mate");
+// try catch ko hatane ke liye use kiya 
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema");
 
 main().then( () => {console.log("connected To DB")}).catch(err => console.log(err));
 
@@ -37,11 +41,25 @@ app.use(express.static(path.join(__dirname , "public")));
 // })
 
 
+
+// validation shema for middlewears
+
+const validateListing = (req,res,next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map( (el) => el.message).join(",");
+       throw new ExpressError(404, errMsg)
+    } else{
+        next()
+    }
+}
+
+
 // Index route 
-app.get("/listings" , async (req,res) => {
+app.get("/listings" , wrapAsync(async (req,res) => {
    const allListing = await Listing.find({});
    res.render("listings/index.ejs" , {allListing})
-})
+})) ;
 
 // Create route 
 
@@ -53,55 +71,75 @@ app.get("/listings/new" , (req, res) => {
 
 // Show route 
 
-app.get("/listings/:id" , async (req,res) => {
+app.get("/listings/:id" , wrapAsync(async (req,res) => {
 
     let {id} = req.params;
    const listing = await Listing.findById(id);
    res.render("listings/show.ejs" , {listing});
-});
+}));
 
 
 // create route 
 
-app.post("/listings" , async (req,res) => {
+app.post("/listings" , validateListing, wrapAsync(async (req,res, next) => {
     // let {title,description, img, price, location , country } = req.body;
-    let newListing = new Listing(req.body.listing);
-   await newListing.save();
-    res.redirect("/listings")
-})
+    // if(!req.body.listing){
+    //     throw new ExpressError(400 , "Send valid data for listing")
+    // }
+      let newListing = new Listing(req.body.listing);
+      await newListing.save();
+      res.redirect("/listings")
+}) );
 
 
 // edit route
-app.get("/listings/:id/edit" , async (req,res) => {
+app.get("/listings/:id/edit" , wrapAsync(async (req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs" , {listing})
-}) 
+}) );
 
 // update route 
 
-app.put("/listings/:id" , async (req,res) => {
+app.put("/listings/:id" , validateListing, wrapAsync( async (req,res) => {
     let {id} = req.params;
    await Listing.findByIdAndUpdate (id , {...req.body.listing});
    res.redirect(`/listings/${id}`)
-})
+}));
 
 
 
 // delete route 
 
-app.delete("/listings/:id" , async (req,res) => {
+app.delete("/listings/:id" , wrapAsync( async (req,res) => {
     let {id} = req.params;
    const deleteListing = await  Listing.findByIdAndDelete(id);
 //    console.log(deleteListing);
    res.redirect("/listings")
+}))
+
+ 
+app.get("/" , (req,res) => {
+    res.send("I am root ")
+});
+
+
+// all route express error
+
+app.all( "*" , (req,res,next) => {
+    next( new ExpressError(404 , "Page not found"))
+})
+
+// error handling 
+
+app.use( (err , req ,res, next) => {
+    let {statusCode = 500 , message = "Somethings went wrong"} = err ;
+    res.status(statusCode).render("listings/error.ejs" , {message})
+    // res.status(statusCode).send(message);
 })
 
 
 
-app.get("/" , (req,res) => {
-    res.send("I am root ")
-});
 
 app.listen(3000, () => {
     console.log("Server is on port 3000")
---
+++ MajorProject/utils/wrapAsync.js
@@ -0,0 +1,14 @@
+// function wrapAsync(fn){
+//     return function(req,res, next) {
+//         fn(req,res,next).catch(err)
+//     }
+// }
+
+
+
+module.exports = (fn) => {
+    return (req,res, next) => {
+        fn(req,res,next).catch(next)
+    }
+}
+
