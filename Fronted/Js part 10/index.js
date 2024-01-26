e3f7a29d974ae5f6e29814e607e94f2256f459d2
ankursoniblogger <ankursoniblogger@gmail.com>
2024-01-26 14:15:12 +0530

    part 10 Assignment question uploaded

--- Fronted/Js part 10/index.js
+++ Fronted/Js part 10/index.js
@@ -1,5 +1,35 @@
-let input = document.querySelector("input")
-input.addEventListener("keypress" , function(event){
-    console.log(event.key);
-    console.log("key pressed");
+// let input = document.querySelector("input")
+// input.addEventListener("keypress" , function(event){
+//     console.log(event.key);
+//     console.log("key pressed");
+// })
+
+// assignment question
+
+// let body = document.querySelector("body");
+// let btn = document.createElement("button");
+// btn.innerText = "Click me";
+// body.appendChild(btn);
+
+// btn.addEventListener("click" , function (){
+//     console.log("btn clicked");
+//     this.style.backgroundColor = "green";
+// });
+
+
+// question 1 
+
+// btn.addEventListener("keypress" , function(){
+//     console.log("mouse was click");
+// })
+
+
+// question 3 
+let input = document.querySelector("input");
+
+input.addEventListener("input" , function(){
+    let h2 = document.querySelector("h2")
+    h2.innerHTML = input.value;
+    console.log(input.value);
+    console.dir(input)
 })
